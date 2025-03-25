1b2ac1f93629008d647f09afa0014d247699f3db
Jamie <1866+treejamie@users.noreply.github.com>
2025-03-25 20:54:10 +0000

    3 - High level transformations (#2)
    lesson 2 is in and done.

---
+++ lib/servy/handler.ex
@@ -0,0 +1,31 @@
+defmodule Servy.Handler do
+
+  def handle(request) do
+    request
+    |> parse
+    |> route
+    |> format_response
+  end
+
+
+  def parse(_request) do
+    # TODO: Parse the request string into a map:
+    %{ method: "GET", path: "/wildthings", resp_body: "" }
+  end
+
+  def route(_conv) do
+    # TODO: Create a new map that also has the response body:
+    %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def format_response(_conv) do
+    # TODO: Use values in the map to create an HTTP response string:
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Bears, Lions, Tigers
+    """
+  end
+end
