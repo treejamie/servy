2450469ba8268b1b21ec107185fbced206c17c0a
Jamie <1866+treejamie@users.noreply.github.com>
2025-03-31 21:13:26 +0100

    12 - Modelling with Structs (#39)
    Added in the conv struct as well as refactoring some things into Servy.Conv

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,5 +1,7 @@
 defmodule Servy.Handler do
 
+  alias Servy.Conv
+
   @moduledoc """
   Servy!
   """
@@ -20,15 +22,15 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def route(%{method: "GET", path: "/wildthings"} = conv) do
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%{method: "GET", path: "/bears"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
     # get the file
     @pages_path
     |> Path.join("about.html")
@@ -36,11 +38,11 @@ defmodule Servy.Handler do
     |> handle_file(conv)
   end
 
-  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(%{path: path} = conv) do
+  def route(%Conv{path: path} = conv) do
     %{ conv | resp_body: "No #{path} here.", status: 404}
   end
 
@@ -57,9 +59,9 @@ defmodule Servy.Handler do
   end
 
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -1,16 +1,18 @@
 defmodule Servy.Plugins do
 
-  def track(%{status: 404, path: path} = conv) do
+  alias Servy.Conv
+
+  def track(%Conv{status: 404, path: path} = conv) do
     IO.puts "Warning #{path} is on the loose"
     conv
   end
-  def track(conv), do: conv
+  def track(%Conv{} = conv), do: conv
 
-  def rewrite_path( %{method: _method, path: "/wildlife"} = conv) do
+  def rewrite_path( %Conv{method: _method, path: "/wildlife"} = conv) do
     %{conv | path: "/wildthings"}
   end
 
-  def rewrite_path(conv), do: conv
+  def rewrite_path(%Conv{} = conv), do: conv
 
-  def log(conv), do: IO.inspect conv
+  def log(%Conv{} = conv), do: IO.inspect conv
 end
