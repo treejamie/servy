23b3cee742dc858cf327a3c3a595d0ae1b9ec2df
Jamie <1866+treejamie@users.noreply.github.com>
2025-03-31 21:51:24 +0100

    13 - Matching Heads & Tails (#40)

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -42,6 +42,10 @@ defmodule Servy.Handler do
     %{ conv | status: 200, resp_body: "Bear #{id}"}
   end
 
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    %{ conv | status: 201, resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}" }
+  end
+
   def route(%Conv{path: path} = conv) do
     %{ conv | resp_body: "No #{path} here.", status: 404}
   end
@@ -132,4 +136,21 @@ Accept: */*
 
 """
 response = Servy.Handler.handle(request)
+IO.puts response
+
+
+
+request = """
+POST /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
+
+name=Baloo&type=Brown
+"""
+
+response = Servy.Handler.handle(request)
+
 IO.puts response
--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -3,17 +3,27 @@ defmodule Servy.Parser do
   alias Servy.Conv
 
   def parse(request) do
+    # get the top of the request and the  params
+    [top, params_strings] = String.split(request, "\n\n")
+
+    # now split the top into a request line and header list
+    [request_line | header_lines] = String.split(top, "\n")
+
     # get the method and path
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first
-      |> String.split(" ")
+    [method, path, _] = String.split(request_line, " ")
+
+    # parse params
+    params = parse_params(params_strings)
 
     # la fin
     %Conv{
       method: method,
       path: path,
+      params: params
     }
+  end
+
+  def parse_params(params) do
+    params |> String.trim |> URI.decode_query
   end
 end
