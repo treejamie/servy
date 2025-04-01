ced4526e1ae130e6b043f15d7a5eab879511448e
treejamie <me@treejamie.com>
2025-04-01 16:33:45 +0100

    Headers map is now in using recursion. Most of the work has been done in the parser module

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -12,18 +12,43 @@ defmodule Servy.Parser do
     # get the method and path
     [method, path, _] = String.split(request_line, " ")
 
+    # now parse the headers
+    headers = parse_headers(header_lines, %{})
+
     # parse params
-    params = parse_params(params_strings)
+    params = parse_params(headers["Content-Type"], params_strings)
+
 
     # la fin
     %Conv{
       method: method,
       path: path,
-      params: params
+      params: params,
+      headers: headers
     }
   end
 
-  def parse_params(params) do
+  def parse_headers([head | tail], headers) do
+
+    # split up the head into key values - this is ok, becasue there's a spec for HTTP headers
+    # and even if this one didn't match, we'd return a 400 Bad Request
+    [key, value] = String.split(head, ": ")
+
+    # now build the headers by adding key and value onto the map
+    headers = Map.put(headers, key, value)
+
+    # and now recurse
+    parse_headers(tail, headers)
+  end
+
+  # fin
+  def parse_headers([], headers), do: headers
+
+
+  def parse_params("application/x-www-form-urlencoded", params) do
     params |> String.trim |> URI.decode_query
   end
+
+  # catch all
+  def parse_params(_, _), do: %{}
 end
