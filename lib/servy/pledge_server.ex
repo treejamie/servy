733030a84a060346cf0c77732bfc0c8c8aab9005
treejamie <me@treejamie.com>
2025-04-11 13:28:33 +0100

    part 2 of 4 is completed

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,16 +1,38 @@
 defmodule Servy.PledgeServer do
 
-  def create_pledge(name, amount) do
-    {:ok, id} = send_pledge_to_service(name, amount)
 
-    # catche the pledge
-    [{"larry", 10}]
+  def listen_loop(state) do
+    IO.puts "\nWaiting for a message..."
+
+    receive do
+      {:create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+
+        # make the new state
+        new_state = [ {name, amount} | state]
+        IO.puts "#{name} pledged #{amount}"
+        IO.puts "new state is #{inspect new_state}"
+
+        # and call the loop again
+        listen_loop(new_state)
+      {sender, :recent_pledges} ->
+        send sender, {:response, state}
+        IO.puts "Sent pledges to #{inspect sender}"
+        listen_loop(state)
+    end
   end
 
-  def recent_pledges do
-   # returns the most recent, pledges
-    [{"larry", 10}]
-  end
+  # def create_pledge(name, amount) do
+  #   {:ok, id} = send_pledge_to_service(name, amount)
+
+  #   # catche the pledge
+  #   [{"larry", 10}]
+  # end
+
+  # def recent_pledges do
+  #  # returns the most recent, pledges
+  #   [{"larry", 10}]
+  # end
 
   defp send_pledge_to_service(_name, _amount) do
 
