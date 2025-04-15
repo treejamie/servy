3d350d706513eeca2aeaa6bc953977a6ecb6b5f4
Jamie <1866+treejamie@users.noreply.github.com>
2025-04-15 22:09:17 +0100

    🎉 30 - Final OTP Application  🎉   (#56)
    Done - that's a wrap

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Servy.MixProject do
   def project do
     [
       app: :servy,
+      descripion: "A humble HTTP server",
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
@@ -14,7 +15,9 @@ defmodule Servy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :eex, :observer, :wx, :runtime_tools]
+      extra_applications: [:logger, :eex, :observer, :wx, :runtime_tools],
+      mod: {Servy, []},
+      env: [port: 3000]
     ]
   end
 
