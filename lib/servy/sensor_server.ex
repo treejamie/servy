818d3a7e7ee2db01429adbbe04477eaa63c75f92
Tree Jamie <me@treejamie.com>
2025-04-15 21:18:47 +0100

    We have a supervisor

--- lib/servy/sensor_server.ex
+++ lib/servy/sensor_server.ex
@@ -2,14 +2,15 @@ defmodule Servy.SensorServer do
 
   @name :sensor_server
 
-  @refresh_interval :timer.seconds(5)
+  @refresh_interval :timer.minutes(2)
 
   use GenServer
 
   # Client Interface
 
-  def start do
-    GenServer.start(__MODULE__, %{}, name: @name)
+  def start_link(interval) do
+    IO.puts "Starting the SensorServer with #{interval} min refresh..."
+    GenServer.start_link(__MODULE__, %{}, name: @name)
   end
 
   def get_sensor_data do
